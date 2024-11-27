7e79157598bc8381256f4b125e3cceb831ad6bc8
Alvaro Caviedes <94495729+CalciferXe@users.noreply.github.com>
2024-11-27 10:32:38 -0500

    V2 Login

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import ProfilePage from './pages/ProfilePage';
 import NotFound from './pages/NotFound';
-import { LoginButton } from './components/Auth/Login';
-import { LogoutButton } from './components/Auth/Logout';
 import AuthWrapper from './components/Auth/AuthWrapper';
+import './styles/globals/App.css';
 
 function App() {
   const { isAuthenticated } = useAuth0();
@@ -16,20 +15,6 @@ function App() {
     <Router>
       <AuthWrapper>
         <div className="App">
-          <header>
-            <h1>Bienvenido a nuestra aplicación</h1>
-            {isAuthenticated ? (
-              <>
-                <LogoutButton />
-                <p>Estás logueado</p>
-              </>
-            ) : (
-              <>
-                <LoginButton />
-                <p>Por favor, inicia sesión</p>
-              </>
-            )}
-          </header>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
-// /src/pages/Home.js
 import React from 'react';
+import { LoginButton } from '../components/Auth/Login';
+import '../styles/pages/Home.css';
+import logo from '../assets/images/image.png';
 
 const Home = () => {
   return (
-    <div>
-      <h2>Home Page</h2>
+    <div className="homepage">
+      <div className="homepage-logo">
+        <img src={logo} alt="RespawnChatter Logo" />
+      </div>
+      <div className="homepage-text">
+        <h1>RespawnChatter</h1>
+        <p>¡Sé parte de esta comunidad y descubre grandes juegos!</p>
+      </div>
+      <button className="login-button">
+        <LoginButton />
+      </button>
     </div>
   );
 };
