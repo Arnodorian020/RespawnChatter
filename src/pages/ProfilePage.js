9a068f4bc4d8465dee45ac678bca2be1d190b452
Alvaro Caviedes <94495729+CalciferXe@users.noreply.github.com>
2024-11-27 11:30:26 -0500

    v0.3

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,10 +1,12 @@
 // /src/pages/ProfilePage.js
 import React from 'react';
+import { Profile } from '../components/Auth/Profile';  // Importamos el componente Profile
 
 const ProfilePage = () => {
   return (
     <div>
       <h2>Profile Page</h2>
+      <Profile />  {/* Aquí se renderiza la información del perfil */}
     </div>
   );
 };
